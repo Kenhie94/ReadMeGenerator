a19479ba4f4df6503df5880a7a4991022870f005
Kenjy Jap <kenhiebusiness@gmail.com>
2024-09-23 15:12:27 -0700

    created questions

--- index.js
+++ index.js
@@ -1,7 +1,78 @@
 // TODO: Include packages needed for this application
+import fs from "fs";
+import inquirer from "inquirer";
+import colors from "colors";
+
+const questionColors = colors.magenta
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: `${questionColors('What is your project title?')}`,
+        name: "projectTitle" 
+      },
+      {
+        type: "input",
+        message: `${questionColors('Provide a short description:')}`,
+        name: "projectDescription"
+      },
+      {
+        type: "checkbox",
+        message: `${questionColors('Add a table of content:')}`,
+        name: "projectTableofContent",
+        choices: ['Installation', 'Usage', 'Credits', 'License']
+      },
+      {
+        type: "input",
+        message: `${questionColors('What are the stepts required to install your project?')}`,
+        name: "projectInstallation",
+      },
+      {
+        type: "input",
+        message: `${questionColors('Provide instructions and examples for use:')}`,
+        name: "projectUsage",
+      },
+      {
+        type: "input",
+        message: `${questionColors('List your collaborators, if any, with links to their GitHub profile:')}`,
+        name: "projectCredit",
+      },
+      {
+        type: "list",
+        message: `${questionColors('Pick a license for your application:')}`,
+        name: "projectLicense",
+        choices: ['Apache License 2.0', 'MIT License', 'Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 'Boost Software License 1.0', 'The Unlicense']
+      },
+      {
+        type: "input",
+        message: `${questionColors('Add a badge:')}`,
+        name: "projectBadge",
+      },
+      {
+        type: "input",
+        message: `${questionColors('Ask for help from other developers:')}`,
+        name: "projectContribute",
+      },
+      {
+        type: "input",
+        message: `${questionColors('Ask to write tests for your applications')}`,
+        name: "projectTest",
+      },
+      {
+        type: "input",
+        message: `${questionColors('What is your GitHub username?')}`,
+        name: "projectAccount",
+      },
+      {
+        type: "input",
+        message: `${questionColors('What is your Email?')}`,
+        name: "projectEmail",
+      },
+    ])
+]
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
